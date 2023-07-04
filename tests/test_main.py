fd92a3ee9492959828c3f7a8616cd6266b68252e
Maxime Beaulieu <maxime.87@hotmail.com>
2023-07-04 15:42:18 -0400

    add basic token tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,13 @@
 ## To Implement
 from src.main import Main
+import os
+import pytest
 
-def test_ok():
-    assert True
+def test_token_present():
+    main = Main()
+    assert main.TOKEN is not None
 
-def test_fail():
-    assert False
+def test_token_missing():
+    del os.environ['OXYGENCS_TOKEN']
+    main = Main()
+    assert pytest.raises(Exception('ERREUR: TOKEN MANQUANT DANS LES VARIABLES DENVIRONNEMENT')) 
